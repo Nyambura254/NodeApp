9dd89a1b741ff2fb440d40db4570d27cc98ba8c3
BonnieNyambura <nyamburabonnie@gmail.com>
2020-06-16 12:28:47 +0300

    yargs commands to add more functionality

--- notes.js
+++ notes.js
@@ -1,5 +1,21 @@
+const { require } = require("yargs");
+
+var fs = require('fs');
 var getNotes = function() {
     return "concerts are fan";
 }
-
+var addNote = function(tilte, body) {}
+var saveNotes = function(notes) {
+    var dataJSON = JSON.stringify(notes);
+    fs.writeFileSync("notes.json", dataJSON);
+}
+var loadNotes = function() {
+    try {
+        var data = fs.readFileSync('notes.jscon');
+        var dataJSON = data.toString();
+        return JSON.parse(dataJSON);
+    } catch (e) {
+        return [];
+    }
+}
 module.exports = getNotes;
